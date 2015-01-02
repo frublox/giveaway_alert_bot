212aeb9c44b526affb5e63ccd8806e743875478b
frublox <darkeneddan@gmail.com>
2015-01-01 19:48:33 -0500

    HUGE amount of changes -- read full text.
    Turned off spellcheck in PyCharm's settings (was really bothering me).
    
    Renamed a bunch of stuff and moved certain tasks into functions. Also added error checking to the reddit login and the SMTP connection.
    
    Got rid of all the globals (except for a global prefs variable, which makes my life a little easier) and just passed the reddit and smtp connection instances to the functions that need them.
    
    Prefs are now a dictionary instead of separate variables. Also added a sleep_time property to config.ini to control how often the bot checks for new posts.
    
    If the bot gets disconnected somehow during operation, it'll try to reconnect instead of crashing.

--- giveaway_alert_bot.py
+++ giveaway_alert_bot.py
@@ -1,31 +1,43 @@
 __author__ = 'frublox'
 
 import praw
+from praw.errors import InvalidUserPass
+
 import smtplib
+from smtplib import SMTPException
+from smtplib import SMTPAuthenticationError
+from smtplib import SMTPHeloError
+
 from email.mime.text import MIMEText
+
 import time
 import ConfigParser
+import sys
 
 
-def read_config():
+def get_prefs():
     """
-    Reads config.ini and sets preferences to their according values.
+    Returns a dictionary of prefs read from config.ini.
 
     :return: None
     """
-
     config_parser = ConfigParser.SafeConfigParser()
     config_parser.read('config.ini')
 
-    global user_agent, password, to_address, from_address
+    prefs_dict = {
+        'user_agent': config_parser.get('bot', 'user_agent'),
+        'sleep_time': config_parser.getint('bot', 'sleep_time'),
+        'to_address': config_parser.get('email', 'to_address'),
+        'from_address': config_parser.get('email', 'from_address'),
+        'password': config_parser.get('email', 'password')
+    }
 
-    user_agent = config_parser.get('bot', 'user_agent')
-    to_address = config_parser.get('email', 'to_address')
-    from_address = config_parser.get('email', 'from_address')
-    password = config_parser.get('email', 'password')
+    print 'Successfully read in prefs from config.ini.'
+
+    return prefs_dict
 
 
-def send_email_alert(submission_url):
+def send_email_alert(smtp_server, submission_url):
     """
     Sends an email alerting of a giveaway at the specified url.
 
@@ -34,42 +46,103 @@ def send_email_alert(submission_url):
     """
     msg = MIMEText('Giveaway happening at {}!'.format(submission_url))
     msg['Subject'] = '/r/dogecoin giveaway alert!'
-    msg['From'] = from_address
-    msg['To'] = to_address
+    msg['From'] = prefs['from_address']
+    msg['To'] = prefs['to_address']
 
-    smpt_server.sendmail(from_address, [to_address], msg.as_string())
+    smtp_server.sendmail(prefs['from_address'], [prefs['to_address']], msg.as_string())
 
-read_config()
-print 'Successfully read in prefs from config.ini.'
 
-smpt_server = smtplib.SMTP('smtp.gmail.com:587')
-smpt_server.ehlo()
-smpt_server.starttls()
-smpt_server.login(from_address, password)
-print 'Successfully connected to SMTP server!'
+def get_reddit_instance():
+    """
+    Creates a PRAW reddit instance, logs in, and then returns that instance.
+    Will exit the program if an error occurs.
 
-reddit = praw.Reddit(user_agent=user_agent)
-reddit.login()
-print 'Successfully connected to reddit!'
+    :return: A logged-in PRAW reddit instance.
+    """
+    reddit = praw.Reddit(user_agent=prefs['user_agent'])
 
-subreddit = reddit.get_subreddit('dogecoin')
+    try:
+        reddit.login()
+    except InvalidUserPass:
+        print 'Error: the login credentials specified in praw.ini are incorrect -- exiting...'
+        sys.exit(1)
 
-already_checked = []  # for posts that have already been alerted
+    print 'Successfully logged into reddit!'
 
-try:
+    return reddit
+
+
+def get_smtp_connection():
+    """
+    Connects to the gmail SMTP server and logs in, returning an SMTP connection instance.
+    Will exit the program if an error occurs.
+
+    :return: A logged-in SMTP connection instance
+    """
+    smtp_server = smtplib.SMTP('smtp.gmail.com:587')
+
+    try:
+        smtp_server.ehlo()
+        smtp_server.starttls()
+        smtp_server.login(prefs['from_address'], prefs['password'])
+    except SMTPAuthenticationError:
+        print 'Error: SMTP server refused to authenticate connection -- perhaps login credentials are incorrect?'
+        print 'Exiting...'
+        sys.exit(1)
+    except SMTPHeloError:
+        print 'Error: SMTP server responded improperly to HELO greeting -- exiting...'
+        sys.exit(1)
+    except SMTPException:
+        print 'Error: An SMTPException occurred when trying to connect to the SMTP server -- exiting...'
+        sys.exit(1)
+
+    print 'Successfully connected and logged in to SMTP server!'
+
+    return smtp_server
+
+
+def check_posts(reddit, smtp_connection):
+    """
+    Checks 10 new posts in /r/dogecoin for a giveaway, and sends an email if one is found.
+
+    :return: None
+    """
+    subreddit = reddit.get_subreddit('dogecoin')
+
+    already_checked = []
+
     while True:
         for submission in subreddit.get_new(limit=10):
             if submission.id in already_checked:
                 continue
 
-            print u'Checking post "{}"...'.format(submission.title)
+            print u'Checking post "{}"'.format(submission.title)
 
             if submission.title.lower().startswith('[giveaway]'):
-                send_email_alert(submission.url)
+                send_email_alert(smtp_connection, submission.url)
                 print 'Giveaway detected!'
 
             already_checked.append(submission.id)
 
-        time.sleep(60)
-finally:
-    smpt_server.quit()
+        time.sleep(prefs['sleep_time'])
+
+
+def main():
+    smtp_connection = None
+
+    while True:
+        try:
+            smtp_connection = get_smtp_connection()
+            reddit = get_reddit_instance()
+            check_posts(reddit, smtp_connection)
+        except IOError:
+            print 'Lost connection to either SMTP server or reddit, attempting to reconnect...'
+            continue
+        finally:
+            if smtp_connection is not None:
+                smtp_connection.quit()
+
+if __name__ == '__main__':
+    global prefs
+    prefs = get_prefs()
+    main()
